a55d0b35429a82fda6214f6ae6f7c041206cb81f
ch <mkmsacc@outlook.com>
2023-07-29 19:10:44 +0700

    Implement hello endpoint

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri("/")
+                .exchange()
+                .expectBody(String.class)
+                .value(s -> assertThat(s).isEqualTo("hello"));
+
+    }
 
 }
